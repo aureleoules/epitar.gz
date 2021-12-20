3d0e4b99d6a7c11cb127fb9f48b573df66688d07
Aurèle Oulès <aurele@oules.com>
2021-12-20 02:37:46 +0100

    module(lrde): Forced url

--- modules/lrde.epita.fr/entrypoint.sh
+++ modules/lrde.epita.fr/entrypoint.sh
@@ -2,17 +2,19 @@ url="https://www.lrde.epita.fr"
 
 # Do not download if folder already exists 
 [ -d '/output/www.lrde.epita.fr' ] || \
-wget \
-    -nc \
-    --no-check-certificate \
-    -r \
-    -np \
-    --no-http-keep-alive \
-    -e robots=off \
-    --content-disposition \
-    --reject="index.html*" \
-    --reject="index.php*" \
-    "$url"
+for p in "" "~akim"; do
+    wget \
+        -nc \
+        --no-check-certificate \
+        -r \
+        -np \
+        --no-http-keep-alive \
+        -e robots=off \
+        --content-disposition \
+        --reject="index.html*" \
+        --reject="index.php*" \
+        "$url/$p"
+done
 
 find -name 'index.php*' -delete
 find -name 'index.html*' -delete
