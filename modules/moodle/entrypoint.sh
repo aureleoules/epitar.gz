b32dec6261b5fb8a0f5d1a5781e54073e8f25bde
Aurele Oules <aurele@oules.com>
2022-04-21 19:22:45 +0200

    fix(module): Moodle private files fix

--- modules/moodle/entrypoint.sh
+++ modules/moodle/entrypoint.sh
@@ -67,7 +67,7 @@ wget \
     -e robots=off \
     --content-disposition \
     --verbose \
-    --reject-regex '(logout|user|submission_files|calendar|theme)' \
+    --reject-regex '(logout|user|submission_files|calendar|theme|pluginfile.php)' \
     "https://moodle.cri.epita.fr"
 
 
